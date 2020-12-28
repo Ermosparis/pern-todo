95eaf512d255d2b444e49023f9cef06157e4dad6
Ermosparis <gayk.abgaryan@gmail.com>
2020-12-28 13:55:35 +0400

    Added db.js to connect database to server and created router

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const pool = require("./db");
+const router = require("./routes/router");
 PORT = process.env.PORT || 5000;
 
-app.use(corse())
+app.use(corse());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT,()=>{
-    console.log(`Server has started on ${PORT}`)
-})
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server has started on ${PORT}`);
+});
---
+++ server/routes/router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+//ROUTES//
+
+//create a todo
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
+module.exports = router;
