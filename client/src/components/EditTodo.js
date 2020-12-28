9f74558d350ae7fff81563f432ca67ced2c33dea
Ermosparis <gayk.abgaryan@gmail.com>
2020-12-28 23:41:41 +0400

    Added Edit modal

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useState } from "react";
+
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-bs-toggle="modal"
+        data-bs-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div
+        className="modal fade"
+        id={`id${todo.todo_id}`}
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id={`id${todo.todo_id}`}>
+                Edit Todo
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                defaultValue={description}
+              />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-warning">
+                Save changes
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EditTodo;
