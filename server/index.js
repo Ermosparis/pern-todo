9f50ddcef6e59eff40be0bbc919b10a14397019a
Ermosparis <gayk.abgaryan@gmail.com>
2020-12-28 17:27:29 +0400

    React set up with Bootstrap

--- server/index.js
+++ server/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const pool = require("./db");
 const router = require("./routes/router");
-PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
